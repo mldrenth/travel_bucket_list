5502b7711147fbfb6b188756ba3724c741452c44
Marijke Drenth <drenth.marijke@googlemail.com>
2021-12-03 16:41:55 +0200

    Added functionality to add new country via browser

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask import Blueprint
 from models.country import Country
 import repositories.country_repository as country_repository
@@ -9,3 +9,24 @@ countries_blueprint = Blueprint("countries", __name__)
 def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", title = "Countries", countries = countries)
+
+#NEW
+#GET '/countries/new'
+@countries_blueprint.route("/countries/new")
+def new_country():
+    return render_template("countries/new.html", title = "Create New Country")
+
+#POST '/countries'
+@countries_blueprint.route("/countries", methods=['POST'])
+def create_country():
+    name = request.form['name']
+    continent = request.form['continent']
+    visited = False
+    if "visited" in request.form.keys():
+        visited = True
+    want_to_visit = False
+    if "want_to_visit" in request.form.keys():
+        want_to_visit = True
+    country = Country(name, continent, want_to_visit, visited)
+    country_repository.save(country)
+    return redirect('/countries')
