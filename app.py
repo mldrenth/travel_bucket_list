2e1279925c5ac22f882f13fc4f8e0784ae79515f
Marijke Drenth <drenth.marijke@googlemail.com>
2021-12-03 14:31:28 +0200

    Countries in database now render to the browser

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-# from controllers.countries_controller import countries_blueprint
+from controllers.countries_controller import countries_blueprint
 
 app = Flask(__name__)
 
-# app.register_blueprint(countries_blueprint)
+app.register_blueprint(countries_blueprint)
 
 @app.route('/')
 def home():
---
+++ controllers/countries_controller.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect
+from flask import Blueprint
+from models.country import Country
+import repositories.country_repository as country_repository
+
+countries_blueprint = Blueprint("countries", __name__)
+
+@countries_blueprint.route("/countries")
+def countries():
+    countries = country_repository.select_all()
+    return render_template("countries/index.html", title = "Countries", countries = countries)
