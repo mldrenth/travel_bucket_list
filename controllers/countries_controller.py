8b758a41250375871951095673572aad53e9f37e
Marijke Drenth <drenth.marijke@googlemail.com>
2021-12-04 17:14:29 +0200

    Addes styling for detailed country view

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, redirect, request
 from flask import Blueprint
 import geonamescache
-gc = geonamescache.GeonamesCache()
 from models.country import Country
 import repositories.country_repository as country_repository
 import repositories.city_repository as city_repository
 
 countries_blueprint = Blueprint("countries", __name__)
+
+gc = geonamescache.GeonamesCache()
 
 @countries_blueprint.route("/countries")
 def countries():
